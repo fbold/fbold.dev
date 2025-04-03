dca41a7a10b58da249f91e4ecce4306c2d759ff0
fbold <vurak.of.sulcan@gmail.com>
2025-04-03 03:20:00 +0200

    got the sphere thing working with cursor pos aberrations in shader, happy w it

--- src/main.js
+++ src/main.js
@@ -1,24 +1,96 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const width = window.innerWidth
+const height = window.innerHeight
+const scene = new THREE.Scene();
+//const camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
+const camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000);
+camera.position.z = Math.max(width, height)
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.SphereGeometry(width / 2, 120, 120);
+
+const vShader = document.getElementById("vertexshader")
+const fShader = document.getElementById("fragmentshader")
+
+var attributes = {
+    displacement: {
+        type: 'f', // a float
+        value: [] // an empty array
+    }
+}
+
+let verts = geometry.getAttribute("position").count;
+
+let values =
+    attributes.displacement.value;
+
+console.log(verts)
+for (let v = 0; v < verts; v++) {
+    values.push((0.5 + Math.min(Math.random(), 1)) * geometry.parameters.radius * 0.1);
+}
+const positions = new Float32Array(values);
+geometry.setAttribute("displacement", new THREE.BufferAttribute(positions, 1))
+
+const material = new THREE.ShaderMaterial({
+    uniforms: {
+        pointer_direction: {
+            value: new THREE.Vector3()
+        },
+        amplitude: {
+            type: 'f',
+            value: 0
+        }
+    },
+    vertexShader: vShader.textContent,
+    fragmentShader: fShader.textContent
+})
+const cube = new THREE.Mesh(geometry, material);
+
+
+scene.add(cube);
+
+cube.position.x = - width / 2 + cube.geometry.parameters.radius / 4
+cube.position.y = - height / 2 + cube.geometry.parameters.radius / 4
+
+let sphereToPointer = new THREE.Vector3()
+window.addEventListener("mousemove", (e) => {
+    console.log("sphereToPointer", sphereToPointer)
+    const worldPos = windowToWorld(e)
+    // sphere pos to mouse pos
+    sphereToPointer = worldPos.sub(cube.position)
+})
+
+
+function windowToWorld(event) {
+    let vec = new THREE.Vector3(); // create once and reuse
+    let pos = new THREE.Vector3(); // create once and reuse
+
+    vec.set(
+        (event.clientX / width) * 2 - 1,
+        - (event.clientY / height) * 2 + 1,
+        0.5,
+    );
+
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    var distance = (width / 4 + cube.position.z - camera.position.z) / vec.z;
+    pos.copy(camera.position).add(vec.multiplyScalar(distance));
+    return pos
+}
+
+
+let frame = 0
+function animate() {
+    frame += 0.03
+    geometry.setAttribute("displacement", new THREE.BufferAttribute(positions, 1))
+    material.uniforms.pointer_direction.value = sphereToPointer;
+    material.uniforms.amplitude.value = frame;
+    renderer.render(scene, camera);
+
+}
