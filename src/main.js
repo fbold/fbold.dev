71882b58c8d07d61c4d962efadaee0710b95bb5e
fbold <vurak.of.sulcan@gmail.com>
2025-04-03 17:13:43 +0200

    unprojection to world space from camera finally working and sort of understood

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ const height = window.innerHeight
 const scene = new THREE.Scene();
 //const camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
 const camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000);
-camera.position.z = Math.max(width, height)
+camera.position.z = 1000
+// camera.position.y = 10
+// camera.position.y = 10
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,7 +15,7 @@ renderer.setAnimationLoop(animate);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(width / 2, 120, 120);
+const geometry = new THREE.SphereGeometry(Math.max(width, height) / 2, 120, 120);
 
 const vShader = document.getElementById("vertexshader")
 const fShader = document.getElementById("fragmentshader")
@@ -51,26 +53,62 @@ const material = new THREE.ShaderMaterial({
     fragmentShader: fShader.textContent
 })
 const cube = new THREE.Mesh(geometry, material);
-
-
 scene.add(cube);
 
-cube.position.x = - width / 2 + cube.geometry.parameters.radius / 4
-cube.position.y = - height / 2 + cube.geometry.parameters.radius / 4
+// Create a vector to store the normalized device coordinates
+//const NDC = new THREE.Vector3(-width / 2, -height / 2, 0); // 0.5 is the depth (z)
+// const ndcNear = new THREE.Vector3(-1, -1, 0.5); // 0.5 is the depth (z)
+// const ndcFar = new THREE.Vector3(-1, -1, 1); // 0.5 is the depth (z)
+//
+// const radius = cube.geometry.parameters.radius / 4
+// ndcNear.unproject(camera).add(camera.position).add(new THREE.Vector3(radius, radius, 0))
+// ndcFar.unproject(camera).add(camera.position).add(new THREE.Vector3(radius, radius, 0))
+
+
+// Using this depth will place it at 0 (within floating error)
+// Since camera is at z=1000 ---- this really doesn't matter though
+// as it is compensated for below
+const depth = 1000;
+
+// This is right at the bottom left corder of view
+// and the z=1 means it is at the far plane of frustum
+// which means dimensions can be given relative to window dimensions
+// since scene is rendered at window dimensions so anything with those dimensions
+// at the far render plane will be true
+const ndc = new THREE.Vector3(-1, -1, 1) // 0.5 is the depth (z)
+// Unproject the NDC point to world space
+ndc.unproject(camera)
+// Compute direction vector from camera to the point
+const dir = ndc.sub(camera.position).normalize();
+
+// Then find the position in world by adding to camera position the direction along
+// that bottom left edge of frustum according to depth
+const worldPos = camera.position.clone().add(dir.multiplyScalar(depth));
+
+const radius = 0//cube.geometry.parameters.radius / 5
+// Adjust object scale to maintain screen size
+// see this: https://discussions.unity.com/t/calculating-perspective-camera-size-at-depth/60559
+// and this chat: https://chatgpt.com/share/67eea440-74c4-800f-b646-1e8182151ed1
+const worldHeightAtDepth = 2 * depth * Math.tan(0.5 * camera.fov * Math.PI / 180);
+const pixelsToWorld = worldHeightAtDepth / Math.max(width, height);
+console.log("WorldPos", worldPos)
+console.log("Scale", pixelsToWorld)
+cube.position.copy(worldPos).add(new THREE.Vector3(radius * pixelsToWorld, radius * pixelsToWorld, 0));
+cube.scale.set(pixelsToWorld, pixelsToWorld, pixelsToWorld);
+
 
 let sphereToPointer = new THREE.Vector3()
 window.addEventListener("mousemove", (e) => {
-    console.log("sphereToPointer", sphereToPointer)
     const worldPos = windowToWorld(e)
     // sphere pos to mouse pos
     sphereToPointer = worldPos.sub(cube.position)
 })
 
 
-function windowToWorld(event) {
-    let vec = new THREE.Vector3(); // create once and reuse
-    let pos = new THREE.Vector3(); // create once and reuse
+let vec = new THREE.Vector3(); // create once and reuse
+let pos = new THREE.Vector3(); // create once and reuse
 
+function windowToWorld(event) {
     vec.set(
         (event.clientX / width) * 2 - 1,
         - (event.clientY / height) * 2 + 1,
@@ -79,14 +117,30 @@ function windowToWorld(event) {
 
     vec.unproject(camera);
     vec.sub(camera.position).normalize();
-    var distance = (width / 4 + cube.position.z - camera.position.z) / vec.z;
-    pos.copy(camera.position).add(vec.multiplyScalar(distance));
+    pos.copy(camera.position).add(vec.multiplyScalar(0.6 * depth));
     return pos
 }
 
+console.log(camera.position)
 
 let frame = 0
+// let alpha = 0.1
+// const pos = new THREE.Vector3()
+// const v1 = new THREE.Vector3()
+//
+// let skip = 0
+
 function animate() {
+    // alpha += 0.0001
+    // skip++
+    //
+    // v1.copy(ndcNear)
+    // pos.copy(v1.lerp(ndcFar, alpha))
+    // cube.position.copy(pos)
+    //
+    // if (skip % 60 === 0)
+    //     console.log(alpha, pos)
+
     frame += 0.03
     geometry.setAttribute("displacement", new THREE.BufferAttribute(positions, 1))
     material.uniforms.pointer_direction.value = sphereToPointer;
@@ -94,3 +148,4 @@ function animate() {
     renderer.render(scene, camera);
 
 }
+//renderer.render(scene, camera);
